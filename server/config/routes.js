d823c1efb5c2fb2d854086e1f5b3c32beb083801
Mario Roaf-Esparza <marioroafesparza@gmail.com>
2016-06-23 18:08:15 -0700

    rename

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,14 +1,14 @@
 //routes
-var listController = require('../list/listController.js');
+
 
 module.exports = function(app) {
 
 	//list routes
 	app.route('/')
 		//get the saved list
-		.get(listController.getList)
+		.get()
 
 		//post to the saved list
-		.post(listController.addItem);
+		.post();
 
 };
